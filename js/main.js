145f59f52f67435f2f04e53772444091ac4fa0f7
deverex <31496483+deverex@users.noreply.github.com>
2019-04-20 12:34:25 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+window.addEventListener('scroll',function(){
+  if(window.scrollY > 150){
+    document.querySelector('#navbar').style.opacity = 0.9;
+  }
+  else{
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+$('#navbar a, .btn').on('click',function(event){
+  if(this.hash !== ''){
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html,body').animate(
+      {
+        scrollTop:$(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
